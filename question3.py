764c945ede5d5ba5fb94d736137493f7f2b668b4
hjg20 <121248033+hjg20@users.noreply.github.com>
2024-01-27 17:27:11 -0500

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,24 +1,31 @@
 def question3_1():
-    # answer = ['string', 'string', 'string']
+    answer = ['string', 'Quantitative', 'string']
     return answer
 
 def question3_2():
+    answer = ['Discrete', 'Qualitative', 'Ordinal']
     return answer
 
 def question3_3():
+    answer = ['Discrete', 'Qualitative', 'Ordinal']
     return answer 
 
 def question3_4():
+    answer = ['Continuous', 'Quantitative', 'Ratio']
     return answer
 
 def question3_5():
+    answer = ['string', 'string', 'string']
     return answer
 
 def question3_6():
+    answer = ['string', 'string', 'string']
     return answer
 
 def question3_7():
+    answer = ['string', 'string', 'string']
     return answer
 
 def question3_8():
+    answer = ['string', 'string', 'string']
     return answer
