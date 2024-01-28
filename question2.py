fba2628670b2d1c5636129f2b48d54311105456b
hjg20 <121248033+hjg20@users.noreply.github.com>
2024-01-28 08:28:51 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,19 +1,24 @@
 def question2_1():
-    answer = ['Discrete', 'Quantitative', 'Ratio. Number of courses is countable and allows ratios, but is not continuous due to it being countably infinite.']
+    # Number of courses is countable and allows ratios, but is not continuous due to it being countably infinite.
+    answer = ['Discrete', 'Quantitative', 'Ratio']
     return answer
 
 def question2_2():
-    answer = ['Continuous', 'Quantitative', 'Ratio. Car speed allows ratios (20mph is half of 40mph), and is continuous due to it being uncountably infinite.']
+    # Car speed allows ratios (20mph is half of 40mph), and is continuous due to it being uncountably infinite.
+    answer = ['Continuous', 'Quantitative', 'Ratio']
     return answer
 
 def question2_3():
-    answer = ['Continuous', 'Quantitative', 'Ratio. Decibels is a continuous scale, has a zero, and allows ratios.']
+    # Decibels is a continuous scale, has a zero, and allows ratios.
+    answer = ['Continuous', 'Quantitative', 'Ratio']
     return answer 
 
 def question2_4():
-    answer = ['Discrete', 'Qualitative', 'Oridnal. The categroies are not mathematical but there is ranking; i.e., a 2 on the scale is not twice as bad as a 4.']
+    # The categroies are not mathematical but there is ranking; i.e., a 2 on the scale is not twice as bad as a 4.
+    answer = ['Discrete', 'Qualitative', 'Oridnal.']
     return answer
 
 def question2_5():
-    answer = ['Discrete', 'Qualitative', 'Nominal. The values are not numerical and there is no ranking involved (i.e., 0001 is not better than 0002).']
+    # The values are not numerical and there is no ranking involved (i.e., 0001 is not better than 0002).
+    answer = ['Discrete', 'Qualitative', 'Nominal.']
     return answer
