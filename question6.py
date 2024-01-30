a16fbb4e09941c33d581002b788ba0b82c4d7b9a
hjg20 <121248033+hjg20@users.noreply.github.com>
2024-01-30 11:54:18 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,21 +1,21 @@
 def question6_1():
     answer = {
-        'equal_width': ['No Change', 'integer']
-        'equal_frequency': ['No Change', 'integer']
+        'equal_width': ['No Change', 9]
+        'equal_frequency': ['No Change', 9]
     }
     return answer
 
 def question6_2():
         answer = {
-        'equal_width': ['string', 'integer'],
-        'equal_frequency': ['string', 'integer']
+        'equal_width': ['Change', 7],
+        'equal_frequency': ['No Change', 9]
     }
     return answer
 
 def question6_3():
         answer = {
-        'equal_width': ['string', 'integer'],
-        'equal_frequency': ['string', 'integer']
+        'equal_width':  ['Change', 2],
+        'equal_frequency': ['No Change', 9]
     }
     return answer 
 
