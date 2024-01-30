a9a8634f75dc4a96f4969916d7f8f562a307bb3e
hjg20 <121248033+hjg20@users.noreply.github.com>
2024-01-30 15:40:21 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -15,7 +15,7 @@ def question2_3():
 
 def question2_4():
     # The categroies are not mathematical but there is ranking; i.e., a 2 on the scale is not twice as bad as a 4.
-    answer = ['Discrete', 'Qualitative', 'Oridnal']
+    answer = ['Discrete', 'Qualitative', 'Ordinal']
     return answer
 
 def question2_5():
