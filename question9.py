6acf74324f9b31854662ccd3caa5bba76c5dca74
hjg20 <121248033+hjg20@users.noreply.github.com>
2024-01-30 11:44:41 -0500

    Update question9.py

--- question9.py
+++ question9.py
@@ -1,18 +1,20 @@
 def question9_1():
-    """
-    answer = string'
-    """
+    answer = 'string'
     return answer
 
 def question9_2():
+    answer = 'string'
     return answer
 
 def question9_3():
+    answer = 'string'
     return answer 
 
 def question9_4():
+    answer = 'string'
     return answer 
 
 def question9_5():
+    answer = 'string'
     return answer 
 
