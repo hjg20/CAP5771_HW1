84f33cd89043e6cbec572cecb24209fc7e9aae5c
hjg20 <121248033+hjg20@users.noreply.github.com>
2024-01-30 19:17:53 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,24 +1,24 @@
 def question2_1():
     # Number of courses is countable and allows ratios, but is not continuous due to it being countably infinite.
-    answer = ['Discrete', 'Quantitative', 'Ratio']
+    answer = ['discrete', 'quantitative', 'ratio']
     return answer
 
 def question2_2():
     # Car speed allows ratios (20mph is half of 40mph), and is continuous due to it being uncountably infinite.
-    answer = ['Continuous', 'Quantitative', 'Ratio']
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer
 
 def question2_3():
     # Decibels is a continuous scale, has a zero, and allows ratios.
-    answer = ['Continuous', 'Quantitative', 'Ratio']
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer 
 
 def question2_4():
     # The categroies are not mathematical but there is ranking; i.e., a 2 on the scale is not twice as bad as a 4.
-    answer = ['Discrete', 'Qualitative', 'Ordinal']
+    answer = ['discrete', 'qualitative', 'ordinal']
     return answer
 
 def question2_5():
     # The values are not numerical and there is no ranking involved (i.e., 0001 is not better than 0002).
-    answer = ['Discrete', 'Qualitative', 'Nominal']
+    answer = ['discrete', 'qualitative', 'nominal']
     return answer
