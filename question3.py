4ac4e13b867a2af7d987f38f54b245e50322456d
hjg20 <121248033+hjg20@users.noreply.github.com>
2024-01-30 21:23:26 -0500

    Update question3.py

--- question3.py
+++ question3.py
@@ -27,5 +27,5 @@ def question3_7():
     return answer
 
 def question3_8():
-    answer = ['Discrete', 'Qualitative', 'Ordinal']
+    answer = ['Discrete', 'Quantitative', 'Ordinal']
     return answer
