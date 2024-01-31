f4c428b8de56be57ada941536a2b8caa5b94adc3
hjg20 <121248033+hjg20@users.noreply.github.com>
2024-01-30 21:14:23 -0500

    Update question3.py

--- question3.py
+++ question3.py
@@ -23,7 +23,7 @@ def question3_6():
     return answer
 
 def question3_7():
-    answer = [1, 2, 3]#Continuous,Quantitative,Ratio
+    answer = []#Continuous,Quantitative,Ratio
     return answer
 
 def question3_8():
