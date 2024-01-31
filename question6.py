0231e3056b74070853f6377cae38798a0693174e
hjg20 <121248033+hjg20@users.noreply.github.com>
2024-01-30 20:29:18 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -14,7 +14,7 @@ def question6_2():
 
 def question6_3():
     answer = {
-        'equal_width':  ['Change', 2],
+        'equal_width': ['Change', 2],
         'equal_frequency': ['No Change', 4]
     }
     return answer 
