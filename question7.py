31d2b88e0f16024416acfb2cdf39aee7119725a9
hjg20 <121248033+hjg20@users.noreply.github.com>
2024-01-30 20:32:52 -0500

    Update question7.py

--- question7.py
+++ question7.py
@@ -7,7 +7,7 @@ def question7_2():
     return answer
 
 def question7_3():
-    answer = [(0.0, 3.0), (3.0, 4.0), (4.0, 5.0), (5.0, 'infinity')]
+    answer = [('-infinity', 3.0), (3.0, 4.0), (4.0, 5.0), (5.0, 'infinity')]
     """
     The representation of inifinity is 'infinity'. 
     bi, ei are floats or 'infinity'. 
