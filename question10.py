26b02265126da0c3dca0a925c17b41f5a904d3c6
hjg20 <121248033+hjg20@users.noreply.github.com>
2024-01-30 20:50:13 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,3 +1,4 @@
+
 def question10_1():
     answer = False
     return answer
@@ -27,7 +28,7 @@ def question10_7():
     return answer
 
 def question10_8():
-    answer = True
+    answer = False
     return answer
 
 def question10_9():
